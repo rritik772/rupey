91139813f26511e1516620bf5db8b7ea7196920e
Ritik Ranjan <rritik772@outlook.com>
2023-05-26 14:59:37 +0530

    test docker container updated

--- docker-mysql.sh
+++ docker-mysql.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+export $(xargs < .env.test)
+
 docker run \
     --name rupey \
-    -e MYSQL_ROOT_PASSWORD=890076 \
-    -e MYSQL_USER=rupey \
-    -e MYSQL_PASSWORD=7788990012 \
-    -e MYSQL_DATABASE=rupey \
+    -e MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
+    -e MYSQL_USER=${MYSQL_USER} \
+    -e MYSQL_PASSWORD=${MYSQL_PASSWORD} \
+    -e MYSQL_DATABASE=${MYSQL_DATABASE} \
     -p 3306:3306 \
     -d mysql:latest
