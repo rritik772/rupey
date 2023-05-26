07ea37f9beb2484bfbbea71017ea1635c3a4336f
Ritik Ranjan <rritik772@outlook.com>
2023-05-26 11:43:40 +0530

    project skeloton

---
+++ src/main/java/com/f15t/rupey/RupeyApplication.java
@@ -0,0 +1,13 @@
+package com.f15t.rupey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RupeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RupeyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/f15t/rupey/RupeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f15t.rupey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RupeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
