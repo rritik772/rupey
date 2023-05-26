e7e5b5dd3b5a164bd4353eee95c7b4a6235c45c0
Ritik Ranjan <rritik772@outlook.com>
2023-05-26 16:25:05 +0530

    repaired onetoone mapping

--- src/main/java/com/f15t/rupey/Entities/Category.java
+++ src/main/java/com/f15t/rupey/Entities/Category.java
@@ -2,7 +2,6 @@ package com.f15t.rupey.Entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
